08e91cd8fc65d0e08ed6de73788f629c07a581ae
ting <ting@gmail.com>
2018-02-21 01:04:24 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/a19310_000/honeyweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/a19310_000/honeyweather/ChooseAreaFragment.java
@@ -109,14 +109,14 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
-                }else if(currentLevel == LEVEL_COUNTY){
+                }/*else if(currentLevel == LEVEL_COUNTY){
                     String weatherId = countyList.get(position).getWeatherId();
                     Intent intent=new Intent(getActivity(), WeatherActivity.class);
                     intent.putExtra("weather_id", weatherId);
                     startActivity(intent);
                     getActivity().finish();
                 }
-                /*else if (currentLevel == LEVEL_COUNTY) {
+                */else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
                     if (getActivity() instanceof MainActivity) {
                         Intent intent = new Intent(getActivity(), WeatherActivity.class);
@@ -129,7 +129,7 @@ public class ChooseAreaFragment extends Fragment {
                         activity.swipeRefresh.setRefreshing(true);
                         activity.requestWeather(weatherId);
                     }
-                }*/
+                }
             }
         });
         backButton.setOnClickListener(new View.OnClickListener() {
